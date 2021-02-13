070afb86eace777bff080becb88736875c70fad1
Karoline Stabell <Karoline Stabell@192.168.0.157>
2021-02-13 09:53:01 +0100

    laget noen settere i Tile

--- project/src/main/java/prosjektkode/Tile.java
+++ project/src/main/java/prosjektkode/Tile.java
@@ -2,4 +2,21 @@ package prosjektkode;
 
 public class Tile {
 
+	private String tile;
+	
+	public void setEmpty() {
+		tile = " ";
+	}
+	
+	public void setMine() {
+		tile = "*";
+	}
+	
+	public void setNumber(int neighborMines) {
+		/* her må vi finne på noe lurt
+		 * og det trengs kalkulasjoner i Board 
+		 * for å finne ut hvor mange neighborMines det er
+		 */
+		tile = String.valueOf(neighborMines);
+	}
 }
