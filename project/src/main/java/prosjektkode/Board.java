1848b70d22884418223274b11d606518ecd5e945
Karoline Stabell <Karoline Stabell@10.22.63.61>
2021-02-18 10:08:32 +0100

    Lagt til liste med antall miner ift. vanskelighetsgrad

--- project/src/main/java/prosjektkode/Board.java
+++ project/src/main/java/prosjektkode/Board.java
@@ -9,15 +9,18 @@ public class Board {
 	private Tile[][] board;
 	private int neighborMines;
 	private final List<Integer> levels = Arrays.asList(10,25,35);
+	private final List<Integer> numberOfMines = Arrays.asList(10,40,100);
 	private int size;
 	
 	
 	public Board(int level) {
-		size = levels.get(level-1);
-		this.board = new Tile[size][size];
-		int numberOfMines = 0;
+		size = levels.get(level-1); // finner str. på brettet utifra vanskelighetsgrad
+		int numOfMines = this.numberOfMines.get(level-1); // antall miner brettet skal ha
+		this.board = new Tile[size][size];    // oppretter brett av todimensjonale arrays
 		
-		for (int y = 0; y < size; y++) {
+		
+		
+		for (int y = 0; y < size; y++) {      
 			for (int x = 0; x < size; x++) {
 				
 			}
