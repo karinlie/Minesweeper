6cfa14b1655db555513d57dee0a393040c06605c
Karin <Karin@DESKTOP-9QQFD1L>
2021-02-18 09:00:29 +0100

    En liste har jeg laget

--- project/src/main/java/prosjektkode/Board.java
+++ project/src/main/java/prosjektkode/Board.java
@@ -1,11 +1,18 @@
 package prosjektkode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Board {
 	
 	private ArrayList<Tile> board;
 	private int neighborMines;
+	private final Array<Integer> levels = new ArrayList<Integer>(Arrays.asList(10,25,35));
+	
+	
+	public Board(int level) {
+		
+	}
 	
 	public void setNeighborMines() {
 		
