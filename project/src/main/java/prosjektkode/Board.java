1a7e3dde071e7e8a0dfdae084df14bb985a1df12
Karin <Karin@DESKTOP-9QQFD1L>
2021-02-18 09:18:49 +0100

    små endringer

--- project/src/main/java/prosjektkode/Board.java
+++ project/src/main/java/prosjektkode/Board.java
@@ -1,5 +1,6 @@
 package prosjektkode;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -8,10 +9,11 @@ public class Board {
 	private ArrayList<Tile> board;
 	private int neighborMines;
 	private final Array<Integer> levels = new ArrayList<Integer>(Arrays.asList(10,25,35));
+	private int size;
 	
 	
 	public Board(int level) {
-		
+		size = levels.get(level-1);
 	}
 	
 	public void setNeighborMines() {
