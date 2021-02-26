287daf45ed9e186cb33498b20561de9019523361
Karoline Stabell <Karoline Stabell@10.22.12.113>
2021-02-26 10:07:41 +0100

    lagt til restart Button og fx:id på level buttons

--- project/src/main/java/prosjektkode/MsApp.java
+++ project/src/main/java/prosjektkode/MsApp.java
@@ -1,6 +1,9 @@
 package prosjektkode;
 
+
+
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,11 +13,20 @@ public class MsApp extends Application {
 	public void start(final Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Minesweeper");
 		Scene game = new Scene(FXMLLoader.load(MsApp.class.getResource("msUI2.fxml")));
-//		Scene menu = new Scene(FXMLLoader.load(MsApp.class.getResource("ChooseLevel.fxml")));
 		primaryStage.setScene(game);
 		primaryStage.show();
 	}
 	
+//	@FXML
+//	public void switchScene(ActionEvent event) throws IOException {
+//		Scene game = new Scene(FXMLLoader.load(MsApp.class.getResource("ChooseLevel.fxml")));
+//		Stage inputWindow = (Stage)((Node)event.getSource()).getScene().getWindow();
+//		
+//		
+//		inputWindow.setScene(game);
+//		inputWindow.show();
+//	}
+//	
 	public static void main(String[] args) {
 		MsApp.launch(args);
 	}
