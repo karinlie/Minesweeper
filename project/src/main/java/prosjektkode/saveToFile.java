0d207b5d9e72cc5335964c3faeee21582e959e9b
Karoline Stabell <Karoline Stabell@10.22.62.232>
2021-03-24 14:54:04 +0100

    endret i saveToFile

--- project/src/main/java/prosjektkode/saveToFile.java
+++ project/src/main/java/prosjektkode/saveToFile.java
@@ -1,12 +1,29 @@
 package prosjektkode;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class saveToFile {
 	/* kan bruke json, som gjør at man kan ha listestrukturer i en fil og så går det bra
 	 * 
 	*/
 	
-	public void save() {
-		
+	public final static String SAVE_FOLDER = "src/main/java/savedGames/";
+	
+	
+	public void save(String filename, Board board) throws FileNotFoundException {
+		try (PrintWriter writer = new PrintWriter(filename)) {
+			writer.println(board.getSize());
+			
+			
+			for (int y = 0; y < board.getSize(); y++) {
+				for (int x = 0; x < board.getSize(); x++) {
+					writer.print(board.getTileAt(x, y).toString());
+				}
+			}
+			writer.println();
+		}
 	}
 	
 }
