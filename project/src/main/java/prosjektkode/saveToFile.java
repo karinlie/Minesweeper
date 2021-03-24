f6e8974881c401ef33ab9ba2d1e6e3221fa5ec47
Karin <Karin@DESKTOP-9QQFD1L>
2021-03-24 14:10:02 +0100

    saveToFile

---
+++ project/src/main/java/prosjektkode/saveToFile.java
@@ -0,0 +1,12 @@
+package prosjektkode;
+
+public class saveToFile {
+	/* kan bruke json, som gjør at man kan ha listestrukturer i en fil og så går det bra
+	 * 
+	*/
+	
+	public void save() {
+		
+	}
+	
+}
